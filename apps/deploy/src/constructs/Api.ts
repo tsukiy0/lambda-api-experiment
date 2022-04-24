f1ef692dc4642a61f2ca8957c5fc9e11cbc70cfd
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-04-24 03:15:18 +0000

    feat: deploy Api with function url and http api

--- apps/deploy/src/constructs/Api.ts
+++ apps/deploy/src/constructs/Api.ts
@@ -1,24 +1,65 @@
 import {
-  DefaultFunction,
-  DefaultFunctionHttpApi,
-} from "@tsukiy0/aws-cdk-tools";
-import { Code, Runtime } from "aws-cdk-lib/aws-lambda";
+  Code,
+  FunctionUrl,
+  FunctionUrlAuthType,
+  HttpMethod,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
+import { Function } from "aws-cdk-lib/aws-lambda";
 import path from "path";
+import { Duration } from "aws-cdk-lib";
+import { RetentionDays } from "aws-cdk-lib/aws-logs";
+import { Cors } from "aws-cdk-lib/aws-apigateway";
+import { StringParameter } from "aws-cdk-lib/aws-ssm";
+import {
+  CorsHttpMethod,
+  HttpApi,
+  PayloadFormatVersion,
+} from "@aws-cdk/aws-apigatewayv2-alpha";
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 
 export class Api extends Construct {
   public constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const fn = new DefaultFunction(this, "Function", {
+    const fn = new Function(this, "Function", {
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(path.resolve(__dirname, "../../../api/dist")),
       handler: "index.handler",
       memorySize: 128,
+      retryAttempts: 0,
+      timeout: Duration.seconds(30),
+      logRetention: RetentionDays.ONE_DAY,
     });
 
-    new DefaultFunctionHttpApi(this, "Api", {
-      fn,
+    const fnUrl = new FunctionUrl(this, "FunctionUrl", {
+      function: fn,
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedMethods: [HttpMethod.ALL],
+        allowedOrigins: Cors.ALL_ORIGINS,
+      },
+    });
+
+    const httpApi = new HttpApi(this, "HttpApi", {
+      defaultIntegration: new HttpLambdaIntegration("DefaultIntegration", fn, {
+        payloadFormatVersion: PayloadFormatVersion.VERSION_1_0,
+      }),
+      corsPreflight: {
+        allowMethods: [CorsHttpMethod.ANY],
+        allowOrigins: Cors.ALL_ORIGINS,
+      },
+    });
+
+    new StringParameter(this, "FunctionUrlParam", {
+      parameterName: "/function/url",
+      stringValue: fnUrl.url,
+    });
+
+    new StringParameter(this, "HttpApi", {
+      parameterName: "function/httpapi/url",
+      stringValue: httpApi.url!,
     });
   }
 }
--- apps/deploy/src/stacks/AppStack.ts
+++ apps/deploy/src/stacks/AppStack.ts
@@ -2,7 +2,6 @@ import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { ExternalStack } from "./ExternalStack";
 import { Api } from "../constructs/Api";
-import { Web } from "../constructs/Web";
 
 export class AppStack extends Stack {
   public constructor(
@@ -15,6 +14,5 @@ export class AppStack extends Stack {
     super(scope, id, props);
 
     new Api(this, "Api");
-    new Web(this, "Web");
   }
 }
