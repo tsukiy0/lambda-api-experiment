e044f51aebe4800016bb9512ae4f50f3cef32f5e
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-04-24 03:21:17 +0000

    fix: remove unused environment

--- apps/deploy/src/index.ts
+++ apps/deploy/src/index.ts
@@ -6,28 +6,12 @@ const app = new App();
 
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region = process.env.CDK_DEFAULT_REGION;
-const environment = Environment.check(process.env.ENVIRONMENT);
-
-const config: Record<
-  Environment,
-  {
-    environment: Environment;
-  }
-> = {
-  dev: {
-    environment,
-  },
-  prod: {
-    environment,
-  },
-};
 
 const external = new ExternalStack(app, "LambdaApiExperimentExternal", {
   env: {
     account,
     region,
   },
-  ...config[environment],
 });
 
 new AppStack(app, "LambdaApiExperimentApp", {
@@ -35,6 +19,5 @@ new AppStack(app, "LambdaApiExperimentApp", {
     account,
     region,
   },
-  ...config[environment],
   external,
 });
